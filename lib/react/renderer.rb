32508ec6b41f9b743247fce6bb2fa1a89a64bc2d
Sam Taylor <sjltaylor@gmail.com>
2014-06-11 13:44:43 +0100

    serverside rendering reflects file changes

--- lib/react/renderer.rb
+++ lib/react/renderer.rb
@@ -1,4 +1,5 @@
 require 'connection_pool'
+require 'digest'
 
 module React
   class Renderer
@@ -9,6 +10,7 @@ module React
       args.assert_valid_keys(:size, :timeout)
       @@react_js = react_js
       @@components_js = components_js
+
       @@pool.shutdown{} if @@pool
       @@pool = ConnectionPool.new(:size => args[:size]||10, :timeout => args[:timeout]||20) { self.new }
     end
@@ -19,17 +21,14 @@ module React
       end
     end
 
-    def self.combined_js
-      @@combined_js ||= <<-CODE
-        var global = global || this;
-        #{@@react_js};
-        React = global.React;
-        #{@@components_js};
-      CODE
-    end
-
     def context
-      @context ||= ExecJS.compile(self.class.combined_js)
+      combined_js = self.class.combined_js
+      #combined_js_digest = Digest::SHA1.new.tap{|d| d << combined_js }
+
+      #return @context if @previous_combined_js_digest == combined_js_digest
+      #@previous_combined_js_digest = combined_js_digest
+      #@context = ExecJS.compile(combined_js)
+      ExecJS.compile(combined_js)
     end
 
     def render(component, args={})
@@ -38,6 +37,7 @@ module React
           return React.renderComponentToString(#{component}(#{args.to_json}));
         }()
       JS
+
       context.eval(jscode).html_safe
     # What should be done here? If we are server rendering, and encounter an error in the JS code,
     # then log it and continue, which will just render the react ujs tag, and when the browser tries
@@ -47,5 +47,18 @@ module React
       ::Rails.logger.error "[React::Renderer] #{e.message}"
     end
 
+    protected
+
+    def self.combined_js
+      c = <<-CODE
+        var global = global || this;
+        #{@@react_js};
+        React = global.React;
+        #{@@components_js};
+      CODE
+
+      ::Rails.logger.info "COMBINED JS SHA: #{Digest::SHA1.new.tap{|d| d << c }.to_s}"
+      c
+    end
   end
 end
